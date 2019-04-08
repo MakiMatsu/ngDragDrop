edd281aa7e262bd47506fcce373befc1eff0d218
Maki Matsunaga <makimatsun@pasonatech.co.jp>
2019-04-09 00:02:45 +0900

    WIP : drag & drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { DragDropComponent } from './drag-drop/drag-drop.component';
+import {AppComponent} from './app.component';
+import {DragDropComponent} from './drag-drop/drag-drop.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DragDropComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, DragDropComponent],
+  imports: [BrowserModule, NgModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, DragDropComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/drag-drop/drag-drop.component.ts
+++ src/app/drag-drop/drag-drop.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+} from '@angular/cdk/drag-drop';
 
 @Component({
-  selector: 'app-drag-drop',
-  templateUrl: './drag-drop.component.html',
-  styleUrls: ['./drag-drop.component.scss']
+  selector: 'app-drag-drop-component',
+  templateUrl: 'drag-drop.component.html',
+  styleUrls: ['drag-drop.component.scss'],
 })
-export class DragDropComponent implements OnInit {
+// @NgModule({
+//   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+// })
+export class DragDropComponent {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
-  constructor() { }
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
-  ngOnInit() {
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
-
 }
